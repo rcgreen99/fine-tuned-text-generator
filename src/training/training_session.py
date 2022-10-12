ba0cc4e5fb4d2f3763023067233a52bfa2992636
Reese Green <rcgreen99@gmail.com>
2022-10-12 14:55:50 -0700

    add outline from imdb with a couple functions written

--- src/training/training_session.py
+++ src/training/training_session.py
@@ -1,5 +1,35 @@
-import torch
+from torch.utils.data import DataLoader
+from transformers import AutoModelForCausalLM
+from src.tolkien_dataset_builder import TolkienDatasetBuilder
+from src.training.training_session_arg_parser import TrainingSessionArgParser
+
+
+class TrainingSession:
+    def __init__(self, args):
+        self.args = args
+
+    def run(self):
+        self.create_datasets()
+        self.create_dataloaders()
+        self.create_model()
+        self.create_trainer()
+        self.trainer.fit()
+
+    def create_datasets(self):
+        builder = TolkienDatasetBuilder(self.args.filename)
+        self.train_dataset, self.val_dataset = builder.build()
+
+    def create_dataloaders(self):
+        pass
+
+    def create_model(self):
+        self.model = AutoModelForCausalLM.from_pretrained("EleutherAI/gpt-neo-125M")
+
+    def create_trainer(self):
+        pass
 
 
 if __name__ == "__main__":
-    pass
+    args = TrainingSessionArgParser().parse_args()
+    session = TrainingSession(args)
+    session.run()
